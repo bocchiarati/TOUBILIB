a4a5f3b1ce80ec0a0f98e465cf79a8802c7a2d2d
suki <88000sofien@gmail.com>
2025-09-16 17:05:24 +0200

    2.2 routage

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -1,13 +1,13 @@
 <?php
 declare(strict_types=1);
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
 use Slim\App;
 use toubilib\api\actions\HomeAction;
+use toubilib\api\actions\RDVAction;
 
 
 return function( App $app): App {
+    $app->get("/rdv", RDVAction::class);
     $app->get('/', HomeAction::class);
 
     return $app;
