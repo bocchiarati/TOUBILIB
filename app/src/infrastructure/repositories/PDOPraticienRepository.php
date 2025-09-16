fd3294acf003fc6fb29f91d9ebc01a1ba0c62a30
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-09-16 17:46:51 +0200

    ajout try/catch aux requetes des praticiens

--- app/src/infrastructure/repositories/PDOPraticienRepository.php
+++ app/src/infrastructure/repositories/PDOPraticienRepository.php
@@ -4,6 +4,7 @@ namespace toubilib\infra\repositories;
 
 
 
+use App\application_core\application\exceptions\DatabaseException;
 use PDO;
 use toubilib\infra\repositories\interface\PraticienRepositoryInterface;
 
@@ -17,28 +18,35 @@ class PDOPraticienRepository implements PraticienRepositoryInterface {
     }
 
     public function getPraticiens() : array {
-        $query = $this->pdo->query("SELECT praticien.id, nom, prenom, ville, email, specialite.libelle as specialite FROM praticien
+        try {
+            $query = $this->pdo->query("SELECT praticien.id, nom, prenom, ville, email, specialite.libelle as specialite FROM praticien
                                           INNER JOIN specialite ON praticien.specialite_id = specialite.id");
-        return $query->fetchAll(PDO::FETCH_ASSOC);
+            return $query->fetchAll(PDO::FETCH_ASSOC);
+        } catch (\Throwable $e) {
+            //todo
+        }
     }
 
     public function getPraticien($id): array {
-        $query = $this->pdo->query("SELECT praticien.nom, praticien.prenom, specialite.libelle as specialite, praticien.email, praticien.telephone FROM praticien
+        try {
+            $query = $this->pdo->query("SELECT praticien.nom, praticien.prenom, specialite.libelle as specialite, praticien.email, praticien.telephone FROM praticien
                                           INNER JOIN specialite ON praticien.specialite_id = specialite.id
                                           WHERE praticien.id = '$id'");
 
-        $moyens_paiement = $this->pdo->query("SELECT moyen_paiement.libelle FROM moyen_paiement
+            $moyens_paiement = $this->pdo->query("SELECT moyen_paiement.libelle FROM moyen_paiement
                                            INNER JOIN praticien2moyen ON moyen_paiement.id = praticien2moyen.moyen_id
                                            WHERE praticien2moyen.praticien_id = '$id'");
 
-        $motifs_visite = $this->pdo->query("SELECT motif_visite.libelle as motif_visite FROM motif_visite
+            $motifs_visite = $this->pdo->query("SELECT motif_visite.libelle as motif_visite FROM motif_visite
                                            INNER JOIN praticien2motif ON motif_visite.id = praticien2motif.motif_id
                                            WHERE praticien2motif.praticien_id = '$id'");
+            $array = $query->fetchAll(PDO::FETCH_ASSOC);
+            $array[0]['moyens_paiement'] = $moyens_paiement->fetchAll(PDO::FETCH_ASSOC);
+            $array[0]['motifs_visite'] = $motifs_visite->fetchAll(PDO::FETCH_ASSOC);
 
-        $array = $query->fetchAll(PDO::FETCH_ASSOC);
-        $array[0]['moyens_paiement'] = $moyens_paiement->fetchAll(PDO::FETCH_ASSOC);
-        $array[0]['motifs_visite'] = $motifs_visite->fetchAll(PDO::FETCH_ASSOC);
-
-        return $array;
+            return $array;
+        } catch (\Throwable $e) {
+            //todo
+        }
     }
 }
