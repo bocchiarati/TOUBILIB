fc9a0ff7dec5e71a4f67ffd0688bfb2da1a8afff
suki <88000sofien@gmail.com>
2025-09-16 17:04:35 +0200

    2.2 Implementation RDVACtion

--- app/config/api.php
+++ app/config/api.php
@@ -2,12 +2,16 @@
 
 use Psr\Container\ContainerInterface;
 use toubilib\api\actions\HomeAction;
+use toubilib\api\actions\RDVAction;
 use toubilib\core\application\usecases\interfaces\ServicePraticienInterface;
 
 return [
     // application
     HomeAction::class=> function (ContainerInterface $c) {
         return new HomeAction($c->get(ServicePraticienInterface::class));
+    },
+    RDVAction::class=> function (ContainerInterface $c) {
+        return new RDVAction($c->get(ServicePraticienInterface::class));
     },
 ];
 
