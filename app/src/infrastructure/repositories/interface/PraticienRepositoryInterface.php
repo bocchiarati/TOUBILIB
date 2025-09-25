c97efbd28b0d5a50270b6fc3bec19e757ffff770
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-09-25 17:50:22 +0200

    ajout fonction getPraticien (oubli)

--- app/src/infrastructure/repositories/interface/PraticienRepositoryInterface.php
+++ app/src/infrastructure/repositories/interface/PraticienRepositoryInterface.php
@@ -4,4 +4,5 @@ namespace toubilib\infra\repositories\interface;
 
 interface PraticienRepositoryInterface {
     public function getPraticiens() : array;
+    public function getPraticien($id): array;
 }
