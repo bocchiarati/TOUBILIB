2502332bd890b306e62acae835a4d7522114e09a
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-09-26 00:00:45 +0200

    correction orthographe et accents

--- app/src/application_core/application/usecases/ServiceRendezVous.php
+++ app/src/application_core/application/usecases/ServiceRendezVous.php
@@ -36,12 +36,12 @@ class ServiceRendezVous implements ServiceRendezVousInterface
             $this->rendezVousRepository->createRdv($dto);
             return [
                 'success' => true,
-                "message" => "RDV Créé"
+                "message" => "RDV cree"
             ];
         } catch (\Throwable $th) {
             return [
                 "success" => false,
-                "message" => "RDV n'a pu etre créer"
+                "message" => "RDV n'a pu etre cree"
             ];
         }
     }
