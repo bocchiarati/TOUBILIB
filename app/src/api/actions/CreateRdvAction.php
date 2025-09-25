44d4e86c0da5bd115ce8a5f41d4bf40efa709fa8
bocchiarati <88000sofien@gmail.com>
2025-09-25 17:35:55 +0200

    gestion erreur création RDV fin

--- app/src/api/actions/CreateRdvAction.php
+++ app/src/api/actions/CreateRdvAction.php
@@ -14,7 +14,28 @@ class CreateRdvAction {
     }
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
-        $response->getBody()->write(json_encode($this->serviceRdv->creerRendezVous(new InputRendezVousDTO("0768e78b-12c8-3694-b89b-d0a7451a8fb1", "e9613aa0-3cdd-38ac-8e1e-2588bc1ce1ca", '2025-12-04 18:30:00', '2025-12-04 18:15:00', 15, "test"))));
+        try {
+            $id_pat = $args['id_pat'] ?? null;
+            $id_prat = $args['id_prat'] ?? null;
+            $date_heure_debut = $args['date_heure_debut'] ?? null;
+            $date_heure_fin = $args['date_heure_fin'] ?? null;
+            $duree = $args['duree'] ?? null;
+            $motif = $args['motif'] ?? null;
+
+            if ($id_prat == null || $id_pat == null || $date_heure_debut == null || $date_heure_fin == null || $duree == null || $motif == null) {
+                $response->getBody()->write(json_encode([
+                    "success" => false,
+                    "message" => "Erreur lors de la création d'un rendez-vous"
+                ]));
+            } else {
+                $response->getBody()->write(json_encode($this->serviceRdv->creerRendezVous(new InputRendezVousDTO($id_pat, $id_prat, $date_heure_debut, $date_heure_fin, $duree, $motif))));
+            }
+        } catch (\Throwable) {
+            $response->getBody()->write(json_encode([
+                "success" => false,
+                "message" => "Erreur lors de la création d'un rendez-vous"
+            ]));
+        }
         return $response->withHeader("Content-Type", "application/json");
     }
 }
