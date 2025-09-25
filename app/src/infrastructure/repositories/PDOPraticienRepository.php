eb496c7b2604d02fd6eb9121a8346aba8b11f0d4
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-09-25 16:47:43 +0200

    refactor fonctions PDORendezVous vers PDOPraticien

--- app/src/infrastructure/repositories/PDOPraticienRepository.php
+++ app/src/infrastructure/repositories/PDOPraticienRepository.php
@@ -24,4 +24,52 @@ class PDOPraticienRepository implements PraticienRepositoryInterface {
             throw new Exception("Erreur lors de la reception des praticiens.");
         }
     }
+
+    public function getPraticien($id): array {
+        try {
+            $query = $this->prati_pdo->query("SELECT praticien.nom, praticien.prenom, specialite.libelle as specialite, praticien.email, praticien.telephone FROM praticien
+                                          INNER JOIN specialite ON praticien.specialite_id = specialite.id
+                                          WHERE praticien.id = '$id'");
+        } catch (\Throwable $e) {
+            throw new Exception("Erreur lors de la reception du praticien.");
+        }
+
+        $array = $query->fetchAll(PDO::FETCH_ASSOC);
+        try {
+            $array[0]['moyens_paiement'] = $this->getMoyenPaiement($id);
+        } catch (\Exception $e) {
+            throw new Exception("Erreur lors de la reception des moyens de paiement.");
+        }
+
+        try {
+            $array[0]['motifs_visite'] = $this->getMotifsVisite($id);
+        } catch (\Exception $e) {
+            throw new Exception("Erreur lors de la reception des motifs de visite.");
+        }
+        return $array;
+    }
+
+    private function getMotifsVisite($id) : array {
+        try {
+            $motifs_visite = $this->prati_pdo->query("SELECT motif_visite.libelle as motif_visite FROM motif_visite
+                                           INNER JOIN praticien2motif ON motif_visite.id = praticien2motif.motif_id
+                                           WHERE praticien2motif.praticien_id = '$id'");
+        } catch (\Throwable $e) {
+            throw new Exception("Erreur lors de la reception des motifs de visite.");
+        }
+
+        return $motifs_visite->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    private function getMoyenPaiement($id) : array {
+        try {
+            $moyens_paiement = $this->prati_pdo->query("SELECT moyen_paiement.libelle FROM moyen_paiement
+                                           INNER JOIN praticien2moyen ON moyen_paiement.id = praticien2moyen.moyen_id
+                                           WHERE praticien2moyen.praticien_id = '$id'");
+        } catch (\Throwable $e) {
+            throw new Exception("Erreur lors de la reception des moyens de paiement.");
+        }
+
+        return $moyens_paiement->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
--- app/src/infrastructure/repositories/PDORendezVousRepository.php
+++ app/src/infrastructure/repositories/PDORendezVousRepository.php
@@ -19,55 +19,6 @@ class PDORendezVousRepository implements RendezVousRepositoryInterface {
         $this->rdv_pdo = $rdv_pdo;
     }
 
-
-    public function getPraticien($id): array {
-        try {
-            $query = $this->prati_pdo->query("SELECT praticien.nom, praticien.prenom, specialite.libelle as specialite, praticien.email, praticien.telephone FROM praticien
-                                          INNER JOIN specialite ON praticien.specialite_id = specialite.id
-                                          WHERE praticien.id = '$id'");
-        } catch (\Throwable $e) {
-            throw new Exception("Erreur lors de la reception du praticien.");
-        }
-
-        $array = $query->fetchAll(PDO::FETCH_ASSOC);
-        try {
-            $array[0]['moyens_paiement'] = $this->getMoyenPaiement($id);
-        } catch (\Exception $e) {
-            throw new Exception("Erreur lors de la reception des moyens de paiement.");
-        }
-
-        try {
-            $array[0]['motifs_visite'] = $this->getMotifsVisite($id);
-        } catch (\Exception $e) {
-            throw new Exception("Erreur lors de la reception des motifs de visite.");
-        }
-        return $array;
-    }
-
-    private function getMotifsVisite($id) : array {
-        try {
-            $motifs_visite = $this->prati_pdo->query("SELECT motif_visite.libelle as motif_visite FROM motif_visite
-                                           INNER JOIN praticien2motif ON motif_visite.id = praticien2motif.motif_id
-                                           WHERE praticien2motif.praticien_id = '$id'");
-        } catch (\Throwable $e) {
-            throw new Exception("Erreur lors de la reception des motifs de visite.");
-        }
-
-        return $motifs_visite->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    private function getMoyenPaiement($id) : array {
-        try {
-            $moyens_paiement = $this->prati_pdo->query("SELECT moyen_paiement.libelle FROM moyen_paiement
-                                           INNER JOIN praticien2moyen ON moyen_paiement.id = praticien2moyen.moyen_id
-                                           WHERE praticien2moyen.praticien_id = '$id'");
-        } catch (\Throwable $e) {
-            throw new Exception("Erreur lors de la reception des moyens de paiement.");
-        }
-
-        return $moyens_paiement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
     public function getCreneauxOccupees(string $debut, string $fin, string $praticien_id): array {
         // FORMAT DATE : YYYY-MM-DD
         if (!$this->estDateValide($debut)) {
