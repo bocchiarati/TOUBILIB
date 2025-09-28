96e9d5177c45d0b23b270a6da7387eda4c8eca87
tuline-leveque <tuline.leveque6@etu.univ-lorraine.fr>
2025-09-29 01:47:52 +0200

    ajout constructeur dans l'entite RendezVous

--- app/src/application_core/domain/entities/rdv/RendezVous.php
+++ app/src/application_core/domain/entities/rdv/RendezVous.php
@@ -12,8 +12,31 @@ class RendezVous {
     private string $date_heure_debut;
     private string $date_heure_fin;
     private string $motif_visite;
+    private string $date_creation;
     private int $duree;
     private int $status;
+
+    public function  __construct(
+        string $id,
+        string $praticien_id,
+        string $patient_id,
+        string $date_heure_debut,
+        int $status,
+        int $duree,
+        string $date_heure_fin,
+        string $date_creation,
+        string $motif_visite
+    ){
+        $this->id = $id;
+        $this->praticien_id = $praticien_id;
+        $this->patient_id = $patient_id;
+        $this->date_heure_debut = $date_heure_debut;
+        $this->status = $status;
+        $this->duree = $duree;
+        $this->date_heure_fin = $date_heure_fin;
+        $this->date_creation = $date_creation;
+        $this->motif_visite = $motif_visite;
+    }
 
     /**
      * @throws Exception
