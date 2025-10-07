29d89c7107dbe24aa50f479051d6437eb141fe4d
tuline-leveque <tuline.leveque@gmail.com>
2025-10-07 18:41:49 +0200

    update(route): PraticienRdvAction
    Refactor PraticiensRdvAction into PraticienAction.

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Slim\App;
 use toubilib\api\actions\AnnulerRdvAction;
 use toubilib\api\actions\CreateRdvAction;
-use toubilib\api\actions\PraticiensRdvAction;
+use toubilib\api\actions\PraticienRdvAction;
 use toubilib\api\actions\PraticiensAction;
 use toubilib\api\actions\PraticienAction;
 use toubilib\api\actions\RdvDetailsAction;
@@ -15,7 +15,7 @@ return function( App $app): App {
 //    GET
     $app->get('/praticiens', PraticiensAction::class);
     $app->get('/praticiens/{id_prat}', PraticienAction::class);
-    $app->get("/praticiens/{id_prat}/rdvs", PraticiensRdvAction::class);
+    $app->get("/praticiens/{id_prat}/rdvs", PraticienRdvAction::class);
     $app->get("/praticiens/{id_prat}/rdvs/{rdv_id}", RdvDetailsAction::class);
 
 //    POST
