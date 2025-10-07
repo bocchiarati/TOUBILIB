5c652cea55ece7826c957c782481ffc77f7c9b22
tuline-leveque <tuline.leveque@gmail.com>
2025-10-07 18:42:24 +0200

    update(repository): RendezVous
    Refactor getCreneauxOccupees into getCreneauxOccupes.

--- app/src/infrastructure/repositories/interface/RendezVousRepositoryInterface.php
+++ app/src/infrastructure/repositories/interface/RendezVousRepositoryInterface.php
@@ -5,7 +5,7 @@ namespace toubilib\infra\repositories\interface;
 use toubilib\core\domain\entities\rdv\RendezVous;
 
 interface RendezVousRepositoryInterface {
-    public function getCreneauxOccupees(string $debut, string $fin, string $praticien_id) : array;
+    public function getCreneauxOccupes(string $debut, string $fin, string $praticien_id) : array;
     public function getRDV(string $id_prat, string $id_rdv) : RendezVous;
     public function createRdv($dto) : void;
     public function annulerRdv($id_rdv);
