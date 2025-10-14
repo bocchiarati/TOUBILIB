ef5c7c2f02196c9759ce29c17eb8249b88ac8d78
tuline-leveque <tuline.leveque@gmail.com>
2025-10-14 18:29:43 +0200

    add(settings): auth
    Ajout de le configuration pour la connexion à la bdd auth.

--- app/config/settings.php
+++ app/config/settings.php
@@ -35,5 +35,13 @@ return [
         $password = $config['pat.password'];
         return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
     },
+
+    'auth.pdo' => function (ContainerInterface $c) {
+        $config = parse_ini_file($c->get('db.config'));
+        $dsn = "{$config['auth.driver']}:host={$config['auth.host']};dbname={$config['auth.database']}";
+        $user = $config['auth.username'];
+        $password = $config['auth.password'];
+        return new \PDO($dsn, $user, $password, [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION]);
+    },
 ];
 
