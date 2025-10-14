5f8d628d8ae3178f7aa00a31cd3fc48e5b5445cf
tuline-leveque <tuline.leveque@gmail.com>
2025-10-14 18:30:57 +0200

    add(DTO): Auth
    Ajout du DTO AuthnDTO.

---
+++ app/src/api/dtos/AuthnDTO.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace toubilib\api\dtos;
+
+
+use Exception;
+
+class AuthnDTO {
+    public function __construct(
+        public readonly string $id,
+        public readonly string $email,
+        public readonly string $password,
+        public readonly string $role,
+    ) {}
+}
