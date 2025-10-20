43509e6631f953f867a00aff3c9fac4c6b8449b0
suki <88000sofien@gmail.com>
2025-10-21 00:53:29 +0200

    feature(action) refresh token
    début d'ajout de la fonction refresh token (TODO)

--- app/src/api/routes.php
+++ app/src/api/routes.php
@@ -8,6 +8,7 @@ use toubilib\api\actions\PraticienRdvAction;
 use toubilib\api\actions\PraticiensAction;
 use toubilib\api\actions\PraticienAction;
 use toubilib\api\actions\RdvDetailsAction;
+use toubilib\api\actions\RefreshAction;
 use toubilib\api\actions\SigninAction;
 use toubilib\api\middlewares\AuthnSigninValidationMiddleware;
 use toubilib\api\middlewares\AuthzAccessRdvDetailMiddleware;
