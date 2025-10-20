631068a19decf77f26d9ce118679e4b426948c03
suki <88000sofien@gmail.com>
2025-10-21 00:49:26 +0200

    update&fix(services) Authn et JWT
    Mise à jour et restructuration des élements pour les mettres à leurs places en ce qui concerne le service et le provider d'authentification

--- app/src/application_core/application/usecases/ServiceAuthn.php
+++ app/src/application_core/application/usecases/ServiceAuthn.php
@@ -1,35 +1,42 @@
 <?php
-
 namespace toubilib\core\application\usecases;
-use toubilib\api\dtos\AuthnDTO;
-use toubilib\api\dtos\InputAuthnDTO;
+
+use toubilib\core\application\usecases\interfaces\AuthnProviderInterface;
 use toubilib\core\application\usecases\interfaces\ServiceAuthnInterface;
-use toubilib\core\exceptions\ConnexionException;
-use toubilib\infra\repositories\interface\AuthnRepositoryInterface;
+use toubilib\api\dtos\InputAuthnDTO;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 class ServiceAuthn implements ServiceAuthnInterface {
-    private AuthnRepositoryInterface $serviceRepositoryInterface;
 
-    public function __construct(AuthnRepositoryInterface $serviceRepositoryInterface) {
-        $this->serviceRepositoryInterface = $serviceRepositoryInterface;
+    private AuthnProviderInterface $userProvider;
+    private string $secretKey;
+
+    public function __construct(AuthnProviderInterface $provider, string $jwtSecret) {
+        $this->userProvider = $provider;
+        $this->secretKey = $jwtSecret;
     }
 
-    public function signin(InputAuthnDTO $user_dto): AuthnDTO {
-        try {
-            $user = $this->serviceRepositoryInterface->getUser($user_dto->email);
-            if(password_verify($user_dto->password, $user->password)) {
-                return new AuthnDTO(
-                    id: $user->id,
-                    email: $user->email,
-                    password: $user->password,
-                    role: $user->role
-                );
-            } else {
-                //401 unauthorized
-                throw new ConnexionException("Erreur lors de la connexion.");
-            }
-        } catch (\Exception $e) {
-            throw new \Exception($e->getMessage());
-        }
+    public function login(InputAuthnDTO $user_dto, string $host) : string {
+
+        // 1. On valide l'utilisateur
+        $user = $this->userProvider->signin($user_dto);
+
+        // 2. On construit le payload
+        $payload = [
+            "iss" => $host,
+            "aud" => $host,
+            "iat" => time(),
+            "exp" => time() + 3600,
+            "sub" => $user->id,
+            "data" => [
+                "email" => $user->email,
+                "role" => $user->role,
+            ]
+        ];
+
+        // 3. On encode et on return
+        return JWT::encode($payload, $this->secretKey, 'HS512');
     }
+
 }
--- app/src/application_core/application/usecases/interfaces/ServiceAuthnInterface.php
+++ app/src/application_core/application/usecases/interfaces/ServiceAuthnInterface.php
@@ -1,10 +1,15 @@
 <?php
-
 namespace toubilib\core\application\usecases\interfaces;
 
-use toubilib\api\dtos\AuthnDTO;
 use toubilib\api\dtos\InputAuthnDTO;
 
 interface ServiceAuthnInterface {
-    public function signin(InputAuthnDTO $user_dto): AuthnDTO;
+
+    /**
+     * Orchestre la connexion.
+     * @param InputAuthnDTO $user_dto Les identifiants (email/mdp)
+     * @param string $host Le nom d'hôte (ex: "api.mondomaine.com")
+     * @return string Le token JWT
+     */
+    public function login(InputAuthnDTO $user_dto, string $host) : string; // Modifié ici
 }
